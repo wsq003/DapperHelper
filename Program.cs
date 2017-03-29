920343b7f9b5f4199fefb5971695298ed8705406
wsq003 <wsq003@gmail.com>
2017-03-29 14:24:09 +0800

    aaa

--- Program.cs
+++ Program.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace DapperHelper
 {
 	static class Program
 	{
+
+		[System.Runtime.InteropServices.DllImport("kernel32.dll")]
+		public static extern bool AttachConsole(int dwProcessId);
+		public const int ATTACH_PARENT_PROCESS = -1;
+
 		/// <summary>
 		/// 应用程序的主入口点。
 		/// </summary>
@@ -23,6 +29,17 @@ namespace DapperHelper
 
 			if (args.Length > 0)
 			{
+				if (args.Length != 3)
+				{
+					string info = "\r\nUsage:\r\n\tDapperHelper db_connection_string, dest_folder_path, code_namespace\r\n";
+
+					AttachConsole(ATTACH_PARENT_PROCESS);
+					Console.WriteLine(info);
+					Console.WriteLine("");
+
+					return;
+				}
+
 				string connStr = args[0];
 				string destPath = args[1];
 				string namespace_ = args[2];
